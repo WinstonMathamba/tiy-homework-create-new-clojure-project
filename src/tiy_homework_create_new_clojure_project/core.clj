171b666bdb7be1cc7b6a1fd964ff73e00d40f2a7
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-11 15:57:05 -0400

    Final push with updates

--- src/tiy_homework_create_new_clojure_project/core.clj
+++ src/tiy_homework_create_new_clojure_project/core.clj
@@ -4,16 +4,16 @@
   ;; I defined the main directly above
   ;; now i'll assign some values to variables
 
-  (def firstName 'Winston')
-  (def lastName 'Mathamba')
+  (def firstName "Winston")
+  (def lastName "Mathamba")
   (def address "Foxwood Dr")
   (def x (+ 45 7))
   (def y (* 8 2))
-  (def hairColor 'Brown')
-  (def shoeColor 'Black')
-  (def favoriteSport 'Football')
-  (def carModel 'Infinity')
-  (def dogName 'Luna')
+  (def hairColor "Brown")
+  (def shoeColor "Black")
+  (def favoriteSport "Football")
+  (def carModel "Infinity")
+  (def dogName "Luna")
 
 
   ;; Now i'll set instructions to output
@@ -26,8 +26,8 @@
   (println shoeColor)
   (println favoriteSport)
   (println carModel)
-  (println dogName)
+  (println dogName))
 
-  )
+
 
 (-main)
